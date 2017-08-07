c3e8a11f9f74e3bef4116350cd47e192c823a5c0
Atte Juvonen <atte.juvonen@futurice.com>
2017-08-07 17:32:13 +0300

    gunicorn config

---
+++ gunicorn_config.py
@@ -0,0 +1,13 @@
+import multiprocessing
+import gevent.monkey
+import psycogreen.gevent
+
+# Enable gevent for PSQL
+gevent.monkey.patch_all()
+psycogreen.gevent.patch_psycopg()
+
+# ### Gunicorn settings BEGIN
+bind = [':5000']
+loglevel = 'warning'
+worker_class = 'gevent'
+# ### Gunicorn settings END

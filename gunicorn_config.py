a8343056869bccf9afe9def9b0d043493d5235a6
Atte Juvonen <atte.juvonen@futurice.com>
2017-08-07 17:35:28 +0300

    Multithreading experiments

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,13 +1,12 @@
-import multiprocessing
-import gevent.monkey
-import psycogreen.gevent
+#import gevent.monkey
+#import psycogreen.gevent
 
 # Enable gevent for PSQL
-gevent.monkey.patch_all()
-psycogreen.gevent.patch_psycopg()
+#gevent.monkey.patch_all()
+#psycogreen.gevent.patch_psycopg()
 
 # ### Gunicorn settings BEGIN
 bind = [':5000']
 loglevel = 'warning'
-worker_class = 'gevent'
+worker_class = 'sync'
 # ### Gunicorn settings END

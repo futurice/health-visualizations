0f9c08288063016649832fbbfe0ed7d273bb1a39
Atte Juvonen <atte.juvonen@futurice.com>
2017-08-07 16:59:31 +0300

    Fixed something

--- app.py
+++ app.py
@@ -24,16 +24,14 @@ def route_test():
 def drugs():
     with db_session(db) as session:
         drugs = session.query(Drug).all()
-
-    return jsonify([d.name for d in drugs]), 200, CONTENT_TYPE
+        return jsonify([d.name for d in drugs]), 200, CONTENT_TYPE
 
 @app.route("/dosage_quotes/<drug>/<dosage>/page/<page>")
 @cache.cached()
 def dosage_quotes(drug, dosage, page):
     with db_session(db) as session:
         quotes = Post.find_dosage_quotes(session, drug, dosage, page)
-
-    return jsonify(quotes), 200, CONTENT_TYPE
+        return jsonify(quotes), 200, CONTENT_TYPE
 
 def find_search_term(session, key):
     return Search_Term.find_drug_or_symptom(session, key)
@@ -45,13 +43,11 @@ def related_quotes(key1, key2, page):
         try:
             res1 = find_search_term(session, key1)
             res2 = find_search_term(session, key2)
+            posts = Post.find_related_quotes(session, res1, res2, page)
+            posts = [str(x).decode('utf-8') for x in posts]
+            return jsonify(posts), 200, CONTENT_TYPE
         except NoResultFound:
             return 'Not found', 404, CONTENT_TYPE
-
-        posts = Post.find_related_quotes(session, res1, res2, page)
-        posts = [str(x).decode('utf-8') for x in posts]
-
-    return jsonify(posts), 200, CONTENT_TYPE
 
 @app.route("/search/<term>")
 @cache.cached()
@@ -59,10 +55,9 @@ def show_drug_or_symptom(term):
     with db_session(db) as session:
         try:
             res = find_search_term(session, term)
+            return jsonify(res.data), 200, CONTENT_TYPE
         except NoResultFound:
             return 'Not found', 404, CONTENT_TYPE
-
-    return jsonify(res.data), 200, CONTENT_TYPE
 
 @app.route("/drugs/<drug>")
 @cache.cached()
@@ -70,10 +65,9 @@ def show_drug(drug):
     with db_session(db) as session:
         try:
             d = Drug.find_drug(session, drug)
+            return jsonify(d.data), 200, CONTENT_TYPE
         except NoResultFound:
             return 'Not found', 404, CONTENT_TYPE
-
-    return jsonify(d.data), 200, CONTENT_TYPE
 
 @app.route("/symptoms")
 @cache.cached()
@@ -81,11 +75,9 @@ def symptoms():
     with db_session(db) as session:
         try:
             symptoms = session.query(Symptom).all()
+            return jsonify([s.name for s in symptoms]), 200, CONTENT_TYPE
         except NoResultFound:
             return 'Not found', 404, CONTENT_TYPE
-
-    return jsonify([s.name for s in symptoms]), 200, CONTENT_TYPE
-
 
 @app.route("/symptoms/<symptom>")
 @cache.cached()
@@ -93,10 +85,11 @@ def show_symptom(symptom):
     with db_session(db) as session:
         try:
             s = Symptom.find_symptom(session, symptom)
+            return jsonify(s.data), 200, CONTENT_TYPE
         except NoResultFound:
             return 'Not found', 404, CONTENT_TYPE
 
-    return jsonify(s.data), 200, CONTENT_TYPE
+
 
 # Resource e.g drugs, symptoms
 @app.route("/most_common/<resource>")
@@ -110,4 +103,4 @@ def common(resource):
             query = session.query(Symptom).order_by(Symptom.data['post_count'].desc())
             results = [(symptom.name, symptom.data['post_count']) for symptom in query.all()]
 
-    return jsonify(results), 200, CONTENT_TYPE
+        return jsonify(results), 200, CONTENT_TYPE
